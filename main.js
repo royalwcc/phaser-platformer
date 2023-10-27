46d222dbab96b2caad5d1c7177b9429722aebbd4
Cody Royal <your@email.here>
2023-10-27 16:21:06 -0600

    phaser setup

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 import './style.css'
+import Phaser from "phaser";
+
+/**@type {Phaser.Core.Config} */
+const config = {
+  type: Phaser.WEBGL,
+  width: 400,
+  height: 400,
+};
+
+const game = new Phaser.Game(config);
