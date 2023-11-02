ecdcb7c3a4be03c9be3eb076ddde7c2d7ae75a86
Cody Royal <your@email.here>
2023-11-02 16:13:56 -0600

    Add physics

--- main.js
+++ main.js
@@ -1,11 +1,94 @@
-import './style.css'
+import "./style.css";
 import Phaser from "phaser";
 
-/**@type {Phaser.Core.Config} */
-const config = {
-  type: Phaser.WEBGL,
-  width: 400,
-  height: 400,
+const PLAYER_ANIMS = {
+	idle: "idle",
+	walk: "walk",
+	run: "run",
+	jump: "jump",
 };
+
+class MainScene extends Phaser.Scene {
+	constructor() {
+		super("main-scene");
+	}
+
+	preload() {
+		this.load.atlas("robot", "robot.png", "robot.json");
+	}
+
+	create() {
+		// const height = this.scale.height;
+		// const width = this.scale.width;
+
+		// object destructuring
+		const { height, width } = this.scale;
+
+		let player = this.physics.add.sprite(
+			width / 2,
+			height / 2,
+			"robot",
+			"character_robot_idle.png"
+		);
+
+		player.setCollideWorldBounds(true);
+    player. setBounce(0.50);
+    
+    // single frame
+		player.anims.create({
+			key: PLAYER_ANIMS.idle,
+			frames: [{ key: "robot", frame: "character_robot_idle.png" }],
+		});
+
+		player.anims.create({
+			key: PLAYER_ANIMS.jump,
+			frames: [{ key: "robot", frame: "character_robot_jump.png" }],
+		});
+
+		// multiple frames
+		player.anims.create({
+			key: PLAYER_ANIMS.run,
+			frames: player.anims.generateFrameNames("robot", {
+				start: 0,
+				end: 2,
+				prefix: "character_robot_run",
+				suffix: ".png",
+			}),
+			frameRate: 10, // frames per second
+			repeat: -1, // infinite repeat
+		});
+
+		player.anims.create({
+			key: PLAYER_ANIMS.walk,
+			frames: player.anims.generateFrameNames("robot", {
+				start: 0,
+				end: 7,
+				prefix: "character_robot_walk",
+				suffix: ".png",
+			}),
+			frameRate: 10, // frames per second
+			repeat: -1, // infinite repeat
+		});
+
+		player.play(PLAYER_ANIMS.jump);
+	}
+
+	update() {}
+}
+
+/** @type {Phaser.Types.Core.GameConfig} */
+const config = {
+	type: Phaser.WEBGL,
+	width: 400,
+	height: 400,
+	scene: [MainScene],
+  physics: {
+    default: "arcade",
+    arcade: {
+        gravity: { y: 290 },      
+    }
+  }
+};
+
 
 const game = new Phaser.Game(config);
